70c1d6c0f3dc2abbd919cd5cd4e5330de0413800
aastha <aasthabarve0@gmail.com>
2023-04-11 20:07:32 +0530

    added: welcome

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,5 +3,6 @@ package com.b.Employee_Wage_Com_Problem;
 public class EmployeeWageComputation {
     public static void main(String args[]) {
         System.out.println("  Welcome to employee wage  ");
+
     }
 }
