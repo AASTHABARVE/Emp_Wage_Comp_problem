9e7addca42c6dc5707c70c5e33f08cb3cdc7cac6
aastha <aasthabarve0@gmail.com>
2023-04-11 23:26:47 +0530

    Added: part time emp and wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -30,4 +30,21 @@ public class EmployeeWageComputation {
         System.out.println("Emp Wage: " + empWage);
 
     }
+
+    {
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 4;
+        else if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage: " + empWage);
+    }
 }
