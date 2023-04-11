3214f13c9497480981ef74286fc99747642c2d9f
aastha <aasthabarve0@gmail.com>
2023-04-11 20:46:17 +0530

    Added: Daily Emp wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,7 @@
 package com.b.Employee_Wage_Com_Problem;
 
+import javax.swing.*;
+
 public class EmployeeWageComputation {
     public static void main(String args[]) {
         System.out.println("  Welcome to employee wage  ");
@@ -13,4 +15,18 @@ public class EmployeeWageComputation {
         else System.out.println("Emp is Absent");
     }
 
+    {
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
+
+    }
 }
